67179e563789c70aaa6517b5090c6e477e0dcc3f
Carlos R. Pasqualini <carlos@carlospasqualini.com.ar>
2017-12-20 10:46:28 -0300

    Post CRUD: Add create new button to view

--- views/post/view.php
+++ views/post/view.php
@@ -23,6 +23,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'method' => 'post',
             ],
         ]) ?>
+        <?= Html::a('Create Post', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= DetailView::widget([
